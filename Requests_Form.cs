7cba2b8a1d32585c9900037140bf4ca9bf85c41a
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2020-10-16 21:30:22 +0530

    Update Requests_Form.cs

--- Requests_Form.cs
+++ Requests_Form.cs
@@ -2,3 +2,18 @@
 
 var dictionary = new Dictionary<string, object>();
 Request.Form.CopyTo(dictionary);
+
+
+
+// redirect users to the url where they came from
+//Controller:
+public ActionResult Index()
+{
+   ViewBag.Referrer = Request.UrlReferrer.LocalPath;
+   //.....
+   return View();
+}
+
+//View (Razor syntax):
+
+<a href="@ViewBag.Referrer">Back</a>
